63162327fe08b566466310a95bd5c7a987a0a675
Abdelrahman Gamal <bebogamal01@gmail.com>
2021-09-28 15:31:48 +0200

    Added MVC to Application

--- Book-Store-App/Book-Store-App/Startup.cs
+++ Book-Store-App/Book-Store-App/Startup.cs
@@ -16,6 +16,8 @@ namespace Book_Store_App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // add support for controllers and views
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,24 @@ namespace Book_Store_App
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("first");
+            //    await next();
+            //    await context.Response.WriteAsync("third");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("second");
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // add endpoints to controllers and set default {Home/Index/id?}
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
